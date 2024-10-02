f6cc2edff0f5242b97e118ecff2646ff55080156
ikalinic <ilija.kalinic@amd.com>
2024-10-02 17:00:51 +0200

    Support SD3 ONNX pipeline and conversion script
    Based on diffusers#8984

--- src/diffusers/__init__.py
+++ src/diffusers/__init__.py
@@ -434,6 +434,7 @@ else:
             "OnnxStableDiffusionPipeline",
             "OnnxStableDiffusionUpscalePipeline",
             "StableDiffusionOnnxPipeline",
+            "OnnxStableDiffusion3Pipeline",
         ]
     )
 
@@ -867,6 +868,7 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
             OnnxStableDiffusionPipeline,
             OnnxStableDiffusionUpscalePipeline,
             StableDiffusionOnnxPipeline,
+            OnnxStableDiffusion3Pipeline,
         )
 
     try:
